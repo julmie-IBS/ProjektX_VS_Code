60db0f102de7d78343684d31bb16252db85b9947
Julian Miedler <j.miedler@kolibri-vision.com>
2023-08-12 01:42:25 +0200

    add motor_handler skeleton

---
+++ codebase_esp32_flightcontroller/include/motor_handler.h
@@ -0,0 +1,36 @@
+#ifndef Motor_Handler_h
+#define Motor_Handler_h
+
+
+#include "arduino.h"
+
+
+
+//KREINDL
+#define GPIO_MOSFET ???
+#define GPIO_ESC_1 ???
+#define GPIO_ESC_2  ???
+#define GPIO_ESC_3 ??????
+#define GPIO_ESC_4 ??????
+
+
+
+class motorhandler  {
+public:
+
+    motorhandler(int i2c_address);
+
+    void armEsc() ;
+    void disarmEsc();
+    void writeEsc(byte*);
+ 
+};
+
+
+
+
+
+
+
+
+#endif
---
+++ codebase_esp32_flightcontroller/src/motor_handler.cpp
@@ -0,0 +1,31 @@
+#include "motor_handler.h"
+
+
+
+class motorhandler  {
+public:
+
+
+    motorhandler()
+    {
+            
+    }
+
+    void armEsc() 
+    {
+
+    }
+
+    void disarmEsc()
+    {
+        
+    }
+    
+    void writeEsc(byte*)
+    {
+        
+    }
+ 
+
+
+};
