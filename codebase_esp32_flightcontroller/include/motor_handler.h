b404adb2e51e6e5594ac0b7c38e80109afdf21f0
Julian Miedler <j.miedler@kolibri-vision.com>
2023-11-16 22:59:29 +0100

    add motorhandler stuff props to kreindl

--- codebase_esp32_flightcontroller/include/motor_handler.h
+++ codebase_esp32_flightcontroller/include/motor_handler.h
@@ -14,20 +14,40 @@
 #define GPIO_ESC_4 ??????
 
 
+#define CLR_MOT_ONE GPIO.out1_w1tc.val |= 1 //Pin32 2^(32-32)
+#define SET_MOT_ONE GPIO.out1_w1ts.val |= 1
+
+#define CLR_MOT_TWO GPIO.out1_w1tc.val |= 2 //Pin33 2^(33-32)
+#define SET_MOT_TWO GPIO.out1_w1ts.val |= 2
+
+#define CLR_MOT_THREE GPIO.out_w1tc |= 67108864 //Pin26 2^26
+#define SET_MOT_THREE GPIO.out_w1ts |= 67108864
+
+#define CLR_MOT_FOUR GPIO.out_w1tc |= 134217728 //Pin27: 2^27
+#define SET_MOT_FOUR GPIO.out_w1ts |= 134217728
+
+
 
 class motorhandler  {
 public:
 
-
-
     motorhandler();
-
     void armEsc();
     void disarmEsc();
-
-    void writeEsc(u_int16_t* thrustValues);
+    void writeToMotors(uint16_t* throttle);
+    
     // min max check
  
+private:
+    void WAIT_1();
+    void WAIT_2();
+    void WAIT_3();
+    uint16_t createPacket(uint16_t throttlee);
+    int m_WaitConstantOne=0;
+    int m_WaitConstantTwo=0;
+    int m_WaitConstantThree=0;
+    
+    
 };
 
 
--- codebase_esp32_flightcontroller/src/motor_handler.cpp
+++ codebase_esp32_flightcontroller/src/motor_handler.cpp
@@ -6,24 +6,101 @@
 
 motorhandler::motorhandler()
 {
+    pinMode(32,OUTPUT);   //32 33 26 27
+    pinMode(33,OUTPUT);
+    pinMode(26,OUTPUT);
+    pinMode(27,OUTPUT);
+}
+
+
+
+
+void motorhandler::WAIT_1()
+{
+   //2.5 us
+   for(int ii=0;ii<WAIT_CONSTANT_ONE;ii++)
+   {
+      asm("nop\n\t");
+      asm("nop\n\t");
+      asm("nop\n\t");
+      asm("nop\n\t");
+      asm("nop\n\t");
+   }
+}
+void motorhandler::WAIT_2()
+{
+   //2.5 us
+   for(int ii=0;ii<WAIT_CONSTANT_TWO;ii++){
+      asm("nop\n\t");
+      asm("nop\n\t");
+      asm("nop\n\t");
+      asm("nop\n\t");
+      asm("nop\n\t");
+   }
+}
+void motorhandler::WAIT_3()
+{
+   //1.67 us
+   for(int ii=0;ii<WAIT_CONSTANT_THREE;ii++){
+      asm("nop\n\t");
+      asm("nop\n\t");
+      asm("nop\n\t");
+      asm("nop\n\t");
+      asm("nop\n\t");
+   }
+}
+
+uint16_t motorhandler::createPacket(uint16_t throttlee)
+{
+    uint16_t  value=0;
+    byte crc=0;
+    value = throttlee << 1;
+    value = value & (~0x0001);   //Tel 0
+    //value = value | (0x0001); //Tel 1
+    crc = (value ^ (value >> 4) ^ (value >> 8)) & 0x0F;
+    value =value << 4;
+    value = value & (~0x000F);
+    value= value | crc;
+    return value;  
+}
+
+
+
+void motorhandler::writeToMotors(uint16_t* throttle){
+      uint16_t SendStream1=createPacket(throttle[0]);
+      uint16_t SendStream2=createPacket(throttle[1]);
+      uint16_t SendStream3=createPacket(throttle[2]);
+      uint16_t SendStream4=createPacket(throttle[3]);
+      uint16_t testArray[] ={1 ,2 ,4 ,8 ,16 ,32 ,64 ,128 ,256 ,512 ,1024 ,2048,4096 ,8192 ,16384 ,32768};
+      
+      //delayMicroseconds(104);
+      noInterrupts();  
+      for(int i=15;i>=0;i--)
+      { 
+
+        SET_MOT_ONE;
+        SET_MOT_TWO;
+        SET_MOT_THREE;
+        SET_MOT_FOUR;
         
+        WAIT_1(); //2.5us, mark for zeros to go down
+        
+        if ((SendStream1 & testArray[i]) == 0)  CLR_MOT_ONE;    //else _asm_("nop\n\t");
+        if ((SendStream2 & testArray[i]) == 0)  CLR_MOT_TWO;    //else _asm_("nop\n\t");
+        if ((SendStream3 & testArray[i]) == 0)  CLR_MOT_THREE;  //else _asm_("nop\n\t");
+        if ((SendStream4 & testArray[i]) == 0)  CLR_MOT_FOUR;   //else _asm_("nop\n\t");
+
+        WAIT_2(); //2.5us //mark for ones to go down
+        CLR_MOT_ONE;
+        CLR_MOT_TWO; 
+        CLR_MOT_THREE; 
+        CLR_MOT_FOUR;    
+
+        WAIT_3(); //1.67 //downtime before next bit   
+
+      }//for
+      interrupts();
+      //delayMicroseconds(104);
 }
-
-void motorhandler::armEsc() 
-{
-
-}
-
-void motorhandler::disarmEsc()
-{
-    
-}
-
-void motorhandler::writeEsc(u_int16_t* thrustValues)
-{
-    
-}
-
-
 
 
