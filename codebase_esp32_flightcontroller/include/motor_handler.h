ed2ac49591f51a6266df261da278b9cc3b178b7b
Julian Miedler <j.miedler@kolibri-vision.com>
2023-11-16 23:00:00 +0100

    add motorhandler stuff

--- codebase_esp32_flightcontroller/include/motor_handler.h
+++ codebase_esp32_flightcontroller/include/motor_handler.h
@@ -6,25 +6,25 @@
 
 
 
-//KREINDL
-#define GPIO_MOSFET ???
-#define GPIO_ESC_1 ???
-#define GPIO_ESC_2  ???
-#define GPIO_ESC_3 ??????
-#define GPIO_ESC_4 ??????
+#define WAIT_CONSTANT_ONE   57
+#define WAIT_CONSTANT_TWO   55
+#define WAIT_CONSTANT_THREE 45
 
+#define GPIO_MOT_ONE         32
+#define CLR_MOT_ONE         GPIO.out1_w1tc.val |= 1 //Pin32 2^(32-32)
+#define SET_MOT_ONE         GPIO.out1_w1ts.val |= 1
 
-#define CLR_MOT_ONE GPIO.out1_w1tc.val |= 1 //Pin32 2^(32-32)
-#define SET_MOT_ONE GPIO.out1_w1ts.val |= 1
+#define GPIO_MOT_TWO        33
+#define CLR_MOT_TWO         GPIO.out1_w1tc.val |= 2 //Pin33 2^(33-32)
+#define SET_MOT_TWO         GPIO.out1_w1ts.val |= 2
 
-#define CLR_MOT_TWO GPIO.out1_w1tc.val |= 2 //Pin33 2^(33-32)
-#define SET_MOT_TWO GPIO.out1_w1ts.val |= 2
+#define GPIO_MOT_THREE      26
+#define CLR_MOT_THREE       GPIO.out_w1tc |= 67108864 //Pin26 2^26
+#define SET_MOT_THREE       GPIO.out_w1ts |= 67108864
 
-#define CLR_MOT_THREE GPIO.out_w1tc |= 67108864 //Pin26 2^26
-#define SET_MOT_THREE GPIO.out_w1ts |= 67108864
-
-#define CLR_MOT_FOUR GPIO.out_w1tc |= 134217728 //Pin27: 2^27
-#define SET_MOT_FOUR GPIO.out_w1ts |= 134217728
+#define GPIO_MOT_FOUR       27
+#define CLR_MOT_FOUR        GPIO.out_w1tc |= 134217728 //Pin27: 2^27
+#define SET_MOT_FOUR        GPIO.out_w1ts |= 134217728
 
 
 
@@ -32,8 +32,6 @@ class motorhandler  {
 public:
 
     motorhandler();
-    void armEsc();
-    void disarmEsc();
     void writeToMotors(uint16_t* throttle);
     
     // min max check
@@ -43,9 +41,7 @@ private:
     void WAIT_2();
     void WAIT_3();
     uint16_t createPacket(uint16_t throttlee);
-    int m_WaitConstantOne=0;
-    int m_WaitConstantTwo=0;
-    int m_WaitConstantThree=0;
+   
     
     
 };
--- codebase_esp32_flightcontroller/src/motor_handler.cpp
+++ codebase_esp32_flightcontroller/src/motor_handler.cpp
@@ -6,10 +6,10 @@
 
 motorhandler::motorhandler()
 {
-    pinMode(32,OUTPUT);   //32 33 26 27
-    pinMode(33,OUTPUT);
-    pinMode(26,OUTPUT);
-    pinMode(27,OUTPUT);
+    pinMode(GPIO_MOT_ONE,OUTPUT);   //32 33 26 27
+    pinMode(GPIO_MOT_TWO,OUTPUT);
+    pinMode(GPIO_MOT_THREE,OUTPUT);
+    pinMode(GPIO_MOT_FOUR,OUTPUT);
 }
 
 
@@ -30,7 +30,8 @@ void motorhandler::WAIT_1()
 void motorhandler::WAIT_2()
 {
    //2.5 us
-   for(int ii=0;ii<WAIT_CONSTANT_TWO;ii++){
+   for(int ii=0;ii<WAIT_CONSTANT_TWO;ii++)
+   {
       asm("nop\n\t");
       asm("nop\n\t");
       asm("nop\n\t");
@@ -41,7 +42,8 @@ void motorhandler::WAIT_2()
 void motorhandler::WAIT_3()
 {
    //1.67 us
-   for(int ii=0;ii<WAIT_CONSTANT_THREE;ii++){
+   for(int ii=0;ii<WAIT_CONSTANT_THREE;ii++)
+   {
       asm("nop\n\t");
       asm("nop\n\t");
       asm("nop\n\t");
@@ -66,41 +68,42 @@ uint16_t motorhandler::createPacket(uint16_t throttlee)
 
 
 
-void motorhandler::writeToMotors(uint16_t* throttle){
-      uint16_t SendStream1=createPacket(throttle[0]);
-      uint16_t SendStream2=createPacket(throttle[1]);
-      uint16_t SendStream3=createPacket(throttle[2]);
-      uint16_t SendStream4=createPacket(throttle[3]);
-      uint16_t testArray[] ={1 ,2 ,4 ,8 ,16 ,32 ,64 ,128 ,256 ,512 ,1024 ,2048,4096 ,8192 ,16384 ,32768};
-      
-      //delayMicroseconds(104);
-      noInterrupts();  
-      for(int i=15;i>=0;i--)
-      { 
+void motorhandler::writeToMotors(uint16_t* throttle)
+{
+    uint16_t SendStream1=createPacket(throttle[0]);
+    uint16_t SendStream2=createPacket(throttle[1]);
+    uint16_t SendStream3=createPacket(throttle[2]);
+    uint16_t SendStream4=createPacket(throttle[3]);
+    uint16_t testArray[] ={1 ,2 ,4 ,8 ,16 ,32 ,64 ,128 ,256 ,512 ,1024 ,2048,4096 ,8192 ,16384 ,32768};
+    
+    //delayMicroseconds(104);
+    noInterrupts();  
+    for(int i=15;i>=0;i--)
+    { 
 
-        SET_MOT_ONE;
-        SET_MOT_TWO;
-        SET_MOT_THREE;
-        SET_MOT_FOUR;
-        
-        WAIT_1(); //2.5us, mark for zeros to go down
-        
-        if ((SendStream1 & testArray[i]) == 0)  CLR_MOT_ONE;    //else _asm_("nop\n\t");
-        if ((SendStream2 & testArray[i]) == 0)  CLR_MOT_TWO;    //else _asm_("nop\n\t");
-        if ((SendStream3 & testArray[i]) == 0)  CLR_MOT_THREE;  //else _asm_("nop\n\t");
-        if ((SendStream4 & testArray[i]) == 0)  CLR_MOT_FOUR;   //else _asm_("nop\n\t");
+    SET_MOT_ONE;
+    SET_MOT_TWO;
+    SET_MOT_THREE;
+    SET_MOT_FOUR;
+    
+    WAIT_1(); //2.5us, mark for zeros to go down
+    
+    if ((SendStream1 & testArray[i]) == 0)  CLR_MOT_ONE;    //else _asm_("nop\n\t");
+    if ((SendStream2 & testArray[i]) == 0)  CLR_MOT_TWO;    //else _asm_("nop\n\t");
+    if ((SendStream3 & testArray[i]) == 0)  CLR_MOT_THREE;  //else _asm_("nop\n\t");
+    if ((SendStream4 & testArray[i]) == 0)  CLR_MOT_FOUR;   //else _asm_("nop\n\t");
 
-        WAIT_2(); //2.5us //mark for ones to go down
-        CLR_MOT_ONE;
-        CLR_MOT_TWO; 
-        CLR_MOT_THREE; 
-        CLR_MOT_FOUR;    
+    WAIT_2(); //2.5us //mark for ones to go down
+    CLR_MOT_ONE;
+    CLR_MOT_TWO; 
+    CLR_MOT_THREE; 
+    CLR_MOT_FOUR;    
 
-        WAIT_3(); //1.67 //downtime before next bit   
+    WAIT_3(); //1.67 //downtime before next bit   
 
-      }//for
-      interrupts();
-      //delayMicroseconds(104);
+    }//for
+    interrupts();
+    //delayMicroseconds(104);
 }
 
 
